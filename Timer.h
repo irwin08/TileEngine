134e978c5d63625a82f4c574a649e7edb761e1d1
Jesse Irwin <j.irwin@surreyschools.ca>
2017-02-27 10:40:33 -0800

    Added timer, attempted to rework collision system - failed

---
+++ Timer.h
@@ -0,0 +1,25 @@
+class Timer
+{
+	public:
+	
+		Timer();
+		
+		void start();
+		void stop();
+		void pause();
+		void unpause();
+		
+		int get_ticks();
+		
+		bool is_started();
+		bool is_paused();
+	
+	private:
+	
+		int startTicks;
+		int pausedTicks;
+		
+		bool paused;
+		bool started;
+	
+};
